2578cc40e54da9abaf81a86f5beb726a1c1b846b
Your Name <you@example.com>
2025-04-03 11:55:20 +0100

    ajout de compnents de l'auth

---
+++ project_frontend/src/app/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthSelectionComponent } from './components/auth-selection/auth-selection.component';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { LoginCompanyComponent } from './components/login-company/login-company.component';
+import { RegisterCompanyComponent } from './components/register-company/register-company.component';
+import { SharedComponent } from './components/shared/shared.component';
+
+
+@NgModule({
+  declarations: [
+    AuthSelectionComponent,
+    LoginUserComponent,
+    RegisterUserComponent,
+    LoginCompanyComponent,
+    RegisterCompanyComponent,
+    SharedComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
---
+++ project_frontend/src/app/auth/components/auth-selection/auth-selection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-selection',
+  templateUrl: './auth-selection.component.html',
+  styleUrls: ['./auth-selection.component.css']
+})
+export class AuthSelectionComponent {
+
+}
